2bbca0739d40aab22d021e6361bcaaab49d865e6
Hooray Hu <304327508@qq.com>
2024-06-18 14:30:15 +0800

    首次提交

---
+++ src/ui-provider/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Locale, StyleProvider, Themes } from '@varlet/ui'
+import useSettingsStore from '@/store/modules/settings'
+
+const settingsStore = useSettingsStore()
+
+watch(() => settingsStore.currentColorScheme, (val) => {
+  if (val === 'light') {
+    StyleProvider(null)
+  }
+  else {
+    StyleProvider(Themes.dark)
+  }
+})
+</script>
+
+<template>
+  <VarLocaleProvider locale="zh-CN" :messages="Locale.zhCN" class="min-h-vh supports-[(min-height:100dvh)]:min-h-dvh">
+    <slot />
+  </VarLocaleProvider>
+</template>
---
+++ src/views/index.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const input = ref('')
+const radio = ref(1)
+const rate = ref(3)
+const toggle = ref(true)
+</script>
+
+<template>
+  <PageLayout :navbar="false" tabbar>
+    <PageMain title="Button">
+      <var-button>默认按钮</var-button>
+      <var-button type="primary">
+        主要按钮
+      </var-button>
+      <var-button type="info">
+        信息按钮
+      </var-button>
+      <var-button type="success">
+        成功按钮
+      </var-button>
+      <var-button type="warning">
+        警告按钮
+      </var-button>
+      <var-button type="danger">
+        危险按钮
+      </var-button>
+    </PageMain>
+    <var-form class="mx-4">
+      <var-input v-model="input" label="用户名" placeholder="请输入用户名" />
+    </var-form>
+    <PageMain title="Radio">
+      <var-radio-group v-model="radio">
+        <var-radio :checked-value="1">
+          单选框 1
+        </var-radio>
+        <var-radio :checked-value="2">
+          单选框 2
+        </var-radio>
+      </var-radio-group>
+    </PageMain>
+    <PageMain title="Rate">
+      <var-rate v-model="rate" />
+    </PageMain>
+    <PageMain title="Switch">
+      <var-switch v-model="toggle" />
+    </PageMain>
+  </PageLayout>
+</template>
